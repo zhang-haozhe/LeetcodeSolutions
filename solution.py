2eebbd28f156f754ca3ddae6b595e02286ebe1e9
Haozhe <zhz19980514@ufl.edu>
2020-08-16 03:22:32 -0400

    Revert "Medium_82"
    This reverts commit baedde3a60df271a5340fc494469c5090cf487a5.

--- solution.py
+++ solution.py
@@ -5,22 +5,13 @@
 #         self.next = next
 class Solution:
     def deleteDuplicates(self, head: ListNode) -> ListNode:
-        curr = head
         prev = ListNode(None)
-
-        while curr:
-            jumpOver = -100
-            while curr and curr.next and curr.val == curr.next.val:
-                # print(jumpOver:=curr.val)
-                jumpOver = curr.val
-                curr = curr.next
+        temp = head
+        while(temp):
+            if prev.val == temp.val:
+                prev.next = temp.next
             else:
-                if curr and not curr.next and curr.val == jumpOver:
-                    prev.next = None
-            if jumpOver == -100:
-                prev.next = curr
-                prev = prev.next
-            if jumpOver == head.val:
-                head = curr.next
-            curr = curr.next
+                prev = temp
+            temp = temp.next
+
         return head
