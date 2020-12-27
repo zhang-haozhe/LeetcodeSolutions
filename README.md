3ff1dcbb318936ce61d7d9b9ed46bc93a627215e
Haozhe <zhz19980514@ufl.edu>
2020-12-28 03:13:46 +0800

    Solved the problem in O(n) time and O(1) space. Singly linked list.

--- README.md
+++ README.md
@@ -15,9 +15,12 @@ Could you do it in O(n) time and O(1) space?
 
 # Result:
 
-Runtime: 84 ms, faster than 47.62% of Python3 online submissions for Palindrome Linked List.
-Memory Usage: 23.7 MB, less than 99.65% of Python3 online submissions for Palindrome Linked List.
+Runtime: 68 ms, faster than 74.37% of Python3 online submissions for Palindrome Linked List.
+Memory Usage: 24.4 MB, less than 37.57% of Python3 online submissions for Palindrome Linked List.
 
 # Solution:
 
-First note that the definition of ListNode has been modified so that there is now a property of "the last node". The code runs through the linked list, links the nodes to the previous nodes and increments the counter by 1. Once done, it iterates the list for _counter_ times and compares if the two nodes, one starting from the end, going backward, and the other starting from the beginning, going forwards, have the same value. If not, return false.
+Time complexity: O(n)
+Space complexity: O(1)
+
+I solved this problem by first reversing the sub-linked list after the middle (the second half). By definition, if the linked list is a palindrome, the first half before the middle point and the reversed second half after the middle point should be identical. Therefore, I locate the middle point and manage to reverse the second half. Then, I compare the two halves in each step.
--- solution.py
+++ solution.py
@@ -1,26 +1,47 @@
 # Definition for singly-linked list.
-class ListNode:
-    def __init__(self, val=0, next=None, last=None):
-        self.val = val
-        self.next = next
-        self.last = last
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
+    def reverseList(self, root, prev):
+        # your code here
+        c = root
+        b = prev
+        a = None
+        default = root
+        while c != None:
+            a = b
+            b = c
+            c = c.next
+            b.next = a
+        default.next = None
+        return b
     def isPalindrome(self, head: ListNode) -> bool:
+        length = 0
         curr = head
-        prev = ListNode()
-        check = head
-        count = 0
-        
-        while curr:
-            if prev != None:
-                curr.last = prev
-            prev = curr
+        while curr != None:
             curr = curr.next
-            count += 1
-        for _ in range(count):
-            # print(prev.val, check.val)
-            if prev.val != check.val:
+            length += 1
+        
+        if length <= 1:
+            return True
+        
+        start = head
+        prev = None
+        constant = 1 if length % 2 == 1 else 0
+        for _ in range(length // 2 + constant):
+            prev = start
+            start = start.next
+        
+        if length // 2 != 1:
+            prev.next = self.reverseList(start, prev)
+        curr = head
+        curr2 = prev.next
+        for _ in range(length // 2):
+            if curr.val == curr2.val:
+                curr = curr.next
+                curr2 = curr2.next
+            else:
                 return False
-            prev = prev.last
-            check = check.next
         return True
