df2b6fd4cb000466daace6a469c603d4ea09ae3e
Haozhe <zhz19980514@ufl.edu>
2020-12-27 14:41:38 +0800

    reverse linked list

--- solution.py
+++ solution.py
@@ -1,18 +1,43 @@
-class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        if len(s) <= 1:
-            return len(s)
-        left = 0
-        right = 0
-        maximum = 1
-        substr = ""
-        while right < len(s):
-            if s[right] not in substr:
-                substr += s[right]
-                right += 1
-            else:
-                left = s.index(s[right], left) + 1
-                right += 1
-                substr = s[left:right]
-            maximum = max(maximum, len(substr))
-        return maximum
+class Node:
+    def __init__(self, data, next = None):
+        self.data = data
+        self.next = next
+
+class LinkedList:
+    def __init__(self, head = None):
+        self.head = head
+
+    def link_the_list(self, the_list):
+        iterator = self.head
+        for i in range (len(the_list)):
+            temp = Node(the_list[i])
+            iterator.next = temp
+            iterator = iterator.next
+    
+    def print_list(self):
+        iterator = self.head
+        while iterator != None:
+            print(iterator.data)
+            iterator = iterator.next
+        
+# Parameters:
+#  list: LinkedList
+# return type: None
+
+def reverseList(list):
+    # your code here
+    c = list.head
+    b = None
+    a = None
+    while c != None:
+        a = b
+        b = c
+        c = c.next
+        b.next = a
+    list.head = b
+    
+linked = LinkedList(Node(5))
+linked.link_the_list([5,5,5])
+
+linked.print_list()
+reverseList(linked)
