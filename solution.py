5492c9d7768797610008790570a89cab34217347
Haozhe <zhz19980514@ufl.edu>
2021-01-27 02:37:18 +0800

    quick sort in python

--- solution.py
+++ solution.py
@@ -1,12 +1,43 @@
-class Solution:
-    def minimumTotal(self, triangle: List[List[int]]) -> int:
-        for i in range(1, len(triangle)):
-            for j in range(len(triangle[i])):
-                if j - 1 < 0:
-                    triangle[i][j] = triangle[i][j] + triangle[i - 1][j]
-                else:
-                    if j == len(triangle[i - 1]):
-                        triangle[i][j] = triangle[i][j] + triangle[i - 1][j - 1]
-                    else:
-                        triangle[i][j] = triangle[i][j] + min(triangle[i - 1][j], triangle[i - 1][j - 1])
-        return min(triangle[-1])
+from math import inf
+
+def quickSort(arr):
+    length = len(arr)
+    if length < 2:
+        return arr
+    
+    # in our case, we assume that the pivot is the first element. while in practice, the choice of the pivot position can greatly influence the performance.
+    pivot = 0
+    frontier = 1
+    biggerNum = -inf
+
+    # below is the partitioning step. each time, compare if the pivot element and the currently iterated element:
+    # if the current is larger than the pivot, record its position.
+    # if the current is smaller than or equal to the pivot, then the frontier moves right. if there is a larger element that has appeared, 
+    # then swap this element and the current element.
+
+    curr = frontier
+
+    while curr < length:
+        if arr[curr] <= arr[pivot]:
+            if biggerNum != -inf:
+                arr[biggerNum], arr[curr] = arr[curr], arr[biggerNum]
+                biggerNum = -inf
+                curr -= 1
+            frontier += 1
+        else:
+            if biggerNum == -inf:
+                biggerNum = curr
+        curr += 1
+    
+    # each time the array is executed, swap the element to the left of the frontier and the pivot
+    arr[pivot], arr[frontier - 1] = arr[frontier - 1], arr[pivot]
+
+    # then we divide the array by the new pivot. respectively do quick sort for the two sub-arrays.
+    leftArr = quickSort(arr[:frontier - 1])
+    rightArr = quickSort(arr[frontier:])
+
+    # at the end, join the sorted sub-arrays.
+    return leftArr + [arr[frontier - 1]] + rightArr
+
+
+print(quickSort([4,3,7,2,9,1,8]))
