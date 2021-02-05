bfff18895a4f372cd4944208bba313c4e22dac91
Haozhe <zhz19980514@ufl.edu>
2021-02-05 19:09:34 +0800

    Minimum Path Sum

--- README.md
+++ README.md
@@ -1,38 +1,30 @@
-# 63. Unique Paths II
+# 64. Minimum Path Sum
 
-A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right, which minimizes the sum of all numbers along its path.
 
-The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
-
-Now consider if some obstacles are added to the grids. How many unique paths would there be?
-
-An obstacle and space is marked as 1 and 0 respectively in the grid.
+Note: You can only move either down or right at any point in time.
 
 Example 1:
 
-Input: obstacleGrid = [[0,0,0],[0,1,0],[0,0,0]]
-Output: 2
-Explanation: There is one obstacle in the middle of the 3x3 grid above.
-There are two ways to reach the bottom-right corner:
+Input: grid = [[1,3,1],[1,5,1],[4,2,1]]
+Output: 7
+Explanation: Because the path 1 → 3 → 1 → 1 → 1 minimizes the sum.
+Example 2:
 
-1. Right -> Right -> Down -> Down
-2. Down -> Down -> Right -> Right
-   Example 2:
-
-Input: obstacleGrid = [[0,1],[0,0]]
-Output: 1
+Input: grid = [[1,2,3],[4,5,6]]
+Output: 12
 
 Constraints:
 
-m == obstacleGrid.length
-n == obstacleGrid[i].length
-1 <= m, n <= 100
-obstacleGrid[i][j] is 0 or 1.
+m == grid.length
+n == grid[i].length
+1 <= m, n <= 200
+0 <= grid[i][j] <= 100
 
 # Result:
 
-Runtime: 60 ms, faster than 66.24% of Python3 online submissions for Triangle.
-Memory Usage: 14.9 MB, less than 95.70% of Python3 online submissions for Triangle.
+Runtime: 92 ms, faster than 91.90% of Python3 online submissions for Minimum Path Sum.
+Memory Usage: 15.6 MB, less than 82.71% of Python3 online submissions for Minimum Path Sum.
 
 # Solution:
 
@@ -41,13 +33,4 @@ Space complexity: O(m _ n)
 
 where m \* n is the size of the original matrix.
 
-I solved this problem in 3 different approaches. The basic gist is that each cell is the sum of the cell above it and the cell to its left. Therefore, 3 methods can be derived out of this thought:
-
-Brute force recursion:
-Each time the function queries value of the right cell and the value of the cell under it. Once it gets to the desired spot, return 1. Then, it sum up all the possibilities. The drawback of this method is that it always re-vissts the queried cell, which significnatly reduces the performance.
-
-Memoization:
-The improved version of brute force. On the basis of it, create a map that keeps track of the value of each cell. However, because of the size of call stack, the performance is still very slow.
-
-Dynamic programming:
-The gist of this method is to consider the grid as a matrix, where each cell contains the number of possibilities to get to the cell. Each cell is determined by the one to its left and the one above it. To help process the data, I added 1 additional row and 1 additional column, because for the first row/column from the original grid, there is no cell to its left/above it for reference.
+Quite similar to 62 and 63. This time it only calculates the minimum between the one from the left and the one from the top. Then the value of the cell is calculated by adding the result to the original value in the grid.
--- solution.py
+++ solution.py
@@ -2,67 +2,24 @@ from typing import List
 
 
 class Solution:
-    """
-    brute force solution. does not pass the run time test.
-    """
-    # def recur(self, obstacleGrid: List[List[int]], lenx, leny, x = 0, y = 0):
-    #     if x == lenx or y == leny or obstacleGrid[x][y] == 1:
-    #         return 0
-    #     elif x == lenx - 1 and y == leny - 1:
-    #         return 1
-    #     return self.recurr(obstacleGrid, lenx, leny, x + 1, y) + self.recurr(obstacleGrid, lenx, leny, x, y + 1)
-    # def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
-    #     return self.recurr(obstacleGrid, len(obstacleGrid), len(obstacleGrid[0]))
-    
-    """
-    memoization. does not pass the run time test.
-    """
-    # def recurr(self, map, obstacleGrid: List[List[int]], lenx, leny, x = 0, y = 0):
-    #     key = str(x) + " " + str(y)
-    #     if key in map:
-    #         return map[key]
+    def minPathSum(self, grid: List[List[int]]) -> int:
+ 
+        matrix = []
+        for _ in range(len(grid) + 1):
+            matrix.append([0] * (len(grid[0]) + 1))
+
+        for x in range(1, len(matrix)):
+            for y in range(1, len(matrix[0])):
+                if x != 1 and y != 1:
+                    matrix[x][y] = grid[x - 1][y - 1] + min(matrix[x - 1][y], matrix[x][y - 1])
+                elif x == 1 and y != 1:
+                    matrix[x][y] = grid[x - 1][y - 1] +  matrix[x][y - 1]
+                elif x != 1 and y == 1:
+                    matrix[x][y] = grid[x - 1][y - 1] +  matrix[x - 1][y]
+                else:
+                    matrix[x][y] = grid[x - 1][y - 1] 
         
-    #     if x == lenx or y == leny or obstacleGrid[x][y] == 1:
-    #         map[key] = 0
-    #         return 0
-    #     elif x == lenx - 1 and y == leny - 1:
-    #         map[key] = 1
-    #         return 1
-    #     return self.recurr(map, obstacleGrid, lenx, leny, x + 1, y) + self.recurr(map, obstacleGrid, lenx, leny, x, y + 1)
-    # def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
-    #     return self.recurr(dict(), obstacleGrid, len(obstacleGrid), len(obstacleGrid[0]))
-
-    """dynamic programming"""
-    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
-
-        if obstacleGrid[-1][-1] == 1:
-            return 0
-
-        model = []
-        for _ in range(len(obstacleGrid) + 1):
-            model.append([0] * (len(obstacleGrid[0]) + 1))
-        
-        model[0][1] = 1
-        # to help processing the data, i am adding an additional row and an additional column. to the original matrix' top and left, respectively.
-        # to initialize the matrix, i set the cell above the first processed cell to 1, such that the initial cell is initialized to 1.
-
-        for x in range(1, len(model)):
-            for y in range(1, len(model[0])):
-                # up = model[x - 1][y]
-                # left = model[x][y - 1]
-
-                model[x][y] += 0 if x - 2 >= 0 and y - 1 >= 0 and obstacleGrid[x - 2][y - 1] == 1 else model[x - 1][y]
-                model[x][y] += 0 if x - 1 >= 0 and y - 2 >= 0 and obstacleGrid[x - 1][y - 2] == 1 else model[x][y - 1]
-                # if x - 2 >= 0 and y - 1 >= 0 and obstacleGrid[x - 2][y - 1] == 1:
-                #     up = 0
-
-                # if x - 1 >= 0 and y - 2 >= 0 and obstacleGrid[x - 1][y - 2] == 1:
-                #     left = 0
-                
-                # model[x][y] = up + left
-        
-        return model[-1][-1]
-
+        return matrix[-1][-1]
 
 sol = Solution()
-print(sol.uniquePathsWithObstacles([[0,0,0],[0,1,0],[0,0,0]]))
+print(sol.minPathSum([[1,3,1],[1,5,1],[4,2,1]]))
