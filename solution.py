133ef4ef6f3a6395dc9e1ce4339cae6f60ccc20a
Haozhe <zhz19980514@ufl.edu>
2021-03-12 05:36:14 +0800

    longest common subsequence

--- solution.py
+++ solution.py
@@ -1,15 +1,16 @@
-class Solution:
-    def minDistance(self, word1: str, word2: str) -> int:
-        dp = []
-        for _ in range(len(word2)+1): dp.append([0]*(len(word1)+1))
-        for i in range(len(word1)+1): dp[0][i] = i
-        for i in range(len(word2)+1): dp[i][0] = i
-        
-        for x in range(1, len(dp)):
-            for y in range(1, len(dp[0])):
-                if word1[y-1] == word2[x-1]:
-                    dp[x][y] = dp[x-1][y-1]
-                else:
-                    dp[x][y] = 1 + min(dp[x-1][y], dp[x][y-1], dp[x-1][y-1])
-        
-        return dp[-1][-1]
+def lcs(str1, str2):
+    # your code here
+    dp = [[0] * (len(str1) + 1) for _ in range(len(str2) + 1)]
+
+    for i in range(1, len(dp)):
+        for j in range(1, len(dp[0])):
+            if str2[i - 1] == str1[j - 1]:
+                dp[i][j] = 1 + dp[i-1][j-1]
+            else:
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+    
+    print(dp)
+    return dp[-1][-1]
+
+print(lcs("abdacbab", "acebfca"))
+# print(lcs("ac", "gfecbfdedae"))
