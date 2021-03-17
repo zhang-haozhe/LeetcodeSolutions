b4d39804981ed18dacc2badbc8e2586218b5de28
Haozhe <zhz19980514@ufl.edu>
2021-03-18 01:47:28 +0800

    24. Swap Nodes in Pairs

--- solution.py
+++ solution.py
@@ -1,15 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    def minDistance(self, word1: str, word2: str) -> int:
-        dp = []
-        for _ in range(len(word2)+1): dp.append([0]*(len(word1)+1))
-        for i in range(len(word1)+1): dp[0][i] = i
-        for i in range(len(word2)+1): dp[i][0] = i
+    def swapPairs(self, head: ListNode) -> ListNode:
+        left = head
+        right = head
+        try:
+            right = head.next
+            left.val, right.val = right.val, left.val
+        except:
+            return head
         
-        for x in range(1, len(dp)):
-            for y in range(1, len(dp[0])):
-                if word1[y-1] == word2[x-1]:
-                    dp[x][y] = dp[x-1][y-1]
-                else:
-                    dp[x][y] = 1 + min(dp[x-1][y], dp[x][y-1], dp[x-1][y-1])
+        while right.next and right.next.next:
+            left = left.next.next
+            right = right.next.next
+            left.val, right.val = right.val, left.val
         
-        return dp[-1][-1]
+        return head
