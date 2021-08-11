bf81a1c7f724c506967bd883ea437991947f1814
Haozhe Zhang <zhz19980514@ufl.edu>
2021-08-11 22:59:11 +0800

    knapsack problem

--- solution.py
+++ solution.py
@@ -1,30 +1,15 @@
-class Solution:
-    def quickSelect(self, k, nums, start, end):
-        if start == end:
-            return nums[start]
-        
-        i, j = start, end
-        pivot = nums[(i + j) // 2]
+def backpack(m, A):
+    # write your code here
+    n = len(A)
+    if sum(A) <= m:
+        return sum(A)
+    dp = [[0] * (m + 1) for _ in range(n + 1)]
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            if A[i - 1] > j:
+                dp[i][j] = dp[i - 1][j]
+            else:
+                dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - A[i - 1]] + A[i - 1])
+    return dp[n][m]
 
-        while i <= j:
-            while i <= j and nums[i] > pivot:
-                i += 1
-            while i <= j and nums[j] < pivot:
-                j -= 1
-            
-            if i <= j:
-                nums[i], nums[j] = nums[j], nums[i]
-                i += 1
-                j -= 1
-        
-        if start + k - 1 <= j:
-            return self.quickSelect(k, nums, start, j)
-        if start + k - 1 >= i:
-            return self.quickSelect(k - (i - start), nums, i, end)
-        
-        return nums[j + 1]
-    
-    def findKthLargest(self, nums: List[int], k: int) -> int:
-        if not nums:
-            return -1
-        return self.quickSelect(k, nums, 0, len(nums) - 1)
+print(backpack(10, [3,4,8,5]))
