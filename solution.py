7f5accbdb23b58a2fa79d7b406ffab33c9aef661
Haozhe Zhang <zhz19980514@ufl.edu>
2021-08-24 17:53:08 +0800

    1143. Longest Common Subsequence

--- solution.py
+++ solution.py
@@ -1,16 +1,12 @@
-def lcs(str1, str2):
-    # your code here
-    dp = [[0] * (len(str1) + 1) for _ in range(len(str2) + 1)]
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        dp = [[0] * (len(text1) + 1) for _ in range(len(text2) + 1)]
 
-    for i in range(1, len(dp)):
-        for j in range(1, len(dp[0])):
-            if str2[i - 1] == str1[j - 1]:
-                dp[i][j] = 1 + dp[i-1][j-1]
-            else:
-                dp[i][j] = max(dp[i-1][j], dp[i][j-1])
-    
-    print(dp)
-    return dp[-1][-1]
+        for i in range(1, len(dp)):
+            for j in range(1, len(dp[0])):
+                if text2[i - 1] == text1[j - 1]:
+                    dp[i][j] = 1 + dp[i-1][j-1]
+                else:
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
 
-print(lcs("abdacbab", "acebfca"))
-# print(lcs("ac", "gfecbfdedae"))
+        return dp[-1][-1]
