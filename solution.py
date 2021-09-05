825d9dd3dca1676eac04ec3e2cb693293718d7e4
Haozhe Zhang <zhz19980514@ufl.edu>
2021-09-05 14:42:38 +0800

    Morris Traversal

--- solution.py
+++ solution.py
@@ -1,36 +1,73 @@
-class Solution:
-    def rev(self, nums, start, end):
-        cnt = 0
-        for i in range(end):
-            if cnt == (end - start) // 2:
-                return
-            nums[start + i], nums[end - i - 1] = nums[end - i - 1], nums[start + i]
-            cnt += 1
-        
-    def nextPermutation(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        dest = -1
-        for i in range(len(nums) - 2, -1, -1):
-            if nums[i] < nums[i + 1]:
-                dest = i
-                break
-        
-        if dest == -1:
-            self.rev(nums, 0, len(nums))
-            return
-        
-        bigger = float('inf')
-        destTwo = 0
+def preorderTraversal(root):
+    values = []
+    now = root 
 
-        for i in range(dest + 1, len(nums)):
-            if nums[i] <= bigger and nums[i] > nums[dest]:
-                destTwo = i
-                bigger = nums[i]
-        
-        nums[destTwo], nums[dest] = nums[dest], nums[destTwo]
-        
-        self.rev(nums, dest + 1, len(nums))
-        
-        
+    while now:
+        if now.left:
+            temp = now.left
+            while temp.right and temp.right != now:
+                temp = temp.now
+            
+            if temp.right == now:
+                temp.right = None
+                now = now.right
+            else:
+                values.append(now.val)
+                temp.right = now
+                now = now.left
+
+        else:
+            values.append(now.val)
+            now = now.right
+    
+    return values
+            
+def inorderTraversal(root):
+    values = []
+    now = root 
+
+    while now:
+        if now.left:
+            temp = now.left
+            while temp.right and temp.right != now:
+                temp = temp.now
+            
+            if temp.right == now:
+                values.append(now.val)
+                temp.right = None
+                now = now.right
+            else:
+                temp.right = now
+                now = now.left
+
+        else:
+            values.append(now.val)
+            now = now.right
+    
+    return values
+            
+def postorderTraversal(root):
+    values = []
+    now = root 
+
+    while now:
+        if now.left:
+            temp = now.left
+            while temp.right and temp.right != now:
+                temp = temp.now
+            
+            if temp.right == now:
+                temp.right = None
+                now = now.right
+            else:
+                values.append(now.val)
+                temp.right = now
+                now = now.left
+
+        else:
+            values.append(now.val)
+            now = now.right
+    
+    values.reverse()
+    return values
+            
