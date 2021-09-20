3a3dd0df51db4f77d0ce08eff6ed69a221415a7b
Haozhe Zhang <zhz19980514@gmail.com>
2021-09-20 16:43:11 +0800

    binary search

--- solution.py
+++ solution.py
@@ -1,15 +1,18 @@
-class Solution:
-    def findPeakElement(self, nums: List[int]) -> int:
-        if not nums:
+def binarySearch(nums, target):
+        # write your code here
+        if len(nums) == 0:
             return -1
         
         start, end = 0, len(nums) - 1
         while start + 1 < end:
             mid = (start + end) // 2
-            if nums[mid] > nums[mid - 1]:
+            if nums[mid] < target:
                 start = mid
             else:
                 end = mid
-        if nums[start] > nums[end]:
+        if nums[start] == target:
             return start
-        return end
+        if nums[end] == target:
+            return end
+        
+        return -1
