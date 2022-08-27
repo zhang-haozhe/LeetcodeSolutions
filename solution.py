dca7efccb898810ed1a179544bf83de507ec595b
Haozhe Zhang <zhz19980514@gmail.com>
2022-08-27 00:20:37 -0700

    31. Next Permutation (new)

--- solution.py
+++ solution.py
@@ -1,36 +1,25 @@
 class Solution:
-    def rev(self, nums, start, end):
-        cnt = 0
-        for i in range(end):
-            if cnt == (end - start) // 2:
-                return
-            nums[start + i], nums[end - i - 1] = nums[end - i - 1], nums[start + i]
-            cnt += 1
         
-    def nextPermutation(self, nums: List[int]) -> None:
+    def nextPermutation(self, nums) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        dest = -1
-        for i in range(len(nums) - 2, -1, -1):
-            if nums[i] < nums[i + 1]:
-                dest = i
-                break
-        
-        if dest == -1:
-            self.rev(nums, 0, len(nums))
-            return
-        
-        bigger = float('inf')
-        destTwo = 0
+        l = len(nums)
+        if l < 2:
+            return nums
 
-        for i in range(dest + 1, len(nums)):
-            if nums[i] <= bigger and nums[i] > nums[dest]:
-                destTwo = i
-                bigger = nums[i]
-        
-        nums[destTwo], nums[dest] = nums[dest], nums[destTwo]
-        
-        self.rev(nums, dest + 1, len(nums))
-        
+        i, j , k = l - 2, l - 1, l -1
+        # find the first increasing position
+        while i >= 0 and nums[i] >= nums[j]:
+            i -= 1
+            j -= 1
+        if i >= 0:
+            # find the first element in [j, end) that is bigger than i and swap nums[i] and nums[k]
+            while nums[k] < nums[i]:
+                k -= 1
+            nums[k], nums[i] = nums[i], nums[k]
+        # reverse [j, end)
+        i, j = j, l -1
+        if i < j:
+            nums[i + 1: j] = nums[i + 1:j][::-1]
         
