4f619f21afbb9d5623f39ce8f6dfb27445c0f62b
Haozhe Zhang <zhz19980514@gmail.com>
2022-11-03 17:10:41 -0700

    updated quick select algo

--- solution.py
+++ solution.py
@@ -1,30 +1,20 @@
 class Solution:
-    def quickSelect(self, k, nums, start, end):
-        if start == end:
-            return nums[start]
-        
-        i, j = start, end
-        pivot = nums[(i + j) // 2]
-
-        while i <= j:
-            while i <= j and nums[i] > pivot:
-                i += 1
-            while i <= j and nums[j] < pivot:
-                j -= 1
-            
-            if i <= j:
-                nums[i], nums[j] = nums[j], nums[i]
-                i += 1
-                j -= 1
-        
-        if start + k - 1 <= j:
-            return self.quickSelect(k, nums, start, j)
-        if start + k - 1 >= i:
-            return self.quickSelect(k - (i - start), nums, i, end)
-        
-        return nums[j + 1]
-    
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        if not nums:
-            return -1
-        return self.quickSelect(k, nums, 0, len(nums) - 1)
+        k = len(nums) - k
+        
+        def quickSelect(l, r):
+            pivot, p = nums[r], l
+            for i in range(l, r):
+                if nums[i] <= pivot:
+                    nums[i], nums[p] = nums[p], nums[i]
+                    p += 1
+            nums[p], nums[r] = nums[r], nums[p]
+            
+            if p > k:
+                return quickSelect(l, p - 1)
+            elif p < k:
+                return quickSelect(p + 1, r)
+            else:
+                return nums[p]
+        
+        return quickSelect(0, len(nums) - 1)
