571499c5b5252a3efc8c9674045f858779e3bb3a
Haozhe Zhang <zhz19980514@gmail.com>
2022-12-12 10:31:58 -0800

    solution 2

--- solution.py
+++ solution.py
@@ -1,7 +1,7 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
         k = len(nums) - k
-        
+
         def quickSelect(l, r):
             pivot, p = nums[r], l
             for i in range(l, r):
@@ -9,12 +9,45 @@ class Solution:
                     nums[i], nums[p] = nums[p], nums[i]
                     p += 1
             nums[p], nums[r] = nums[r], nums[p]
-            
+
             if p > k:
                 return quickSelect(l, p - 1)
             elif p < k:
                 return quickSelect(p + 1, r)
             else:
                 return nums[p]
-        
+
         return quickSelect(0, len(nums) - 1)
+
+
+# solution 2
+class Solution:
+    def quickSelect(self, k, nums, start, end):
+        if start == end:
+            return nums[start]
+
+        left, right = start, end
+        pivot = nums[(left + right) // 2]
+
+        while left <= right:
+            while left <= right and nums[left] > pivot:
+                left += 1
+            while left <= right and nums[right] < pivot:
+                right -= 1
+
+            if left <= right:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
+                right -= 1
+
+        if start + k - 1 <= right:
+            return self.quickSelect(k, nums, start, right)
+        if start + k - 1 >= left:
+            return self.quickSelect(k - (left - start), nums, left, end)
+
+        return nums[right + 1]
+
+    def findKthLargest(self, nums, k) -> int:
+        if not nums:
+            return -1
+        return self.quickSelect(k, nums, 0, len(nums) - 1)
